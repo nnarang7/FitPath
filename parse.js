ad946c529658c20103be602b397befc2b563fef5
Summer Yue <yutingyue514@gmail.com>
2015-09-06 02:14:11 -0400

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,10 +1,25 @@
   //"2015-09-07T17:00:00-04:00"
 function parse(dtObject)
-{
-	var m_time = (dtObject.split("T"))[1];
-	var hour = (m_time.split(":"))[0];
-	var minutes = (m_time.split(":"))[1];
-	var re_time = hour + ":" + minutes;
-	return re_time;
-}
+		{			
+			if (dtObject.split(" ").length == 1)
+			{
+				var m_time = (dtObject.split("T"))[1];
+				var hour = (m_time.split(":"))[0];
+				var minutes = (m_time.split(":"))[1];
+				var re_time = hour + ":" + minutes;
+				return re_time;
+			}
+			else
+			{
+				console.log((dtObject.split(" ")));
+				var m_time = ((dtObject.split(" "))[4]);
+				console.log(m_time);
+				var hour = (m_time.split(":"))[0];
+				var minutes = (m_time.split(":"))[1];
+				var re_time = hour + ":" + minutes;
+				return re_time;
+			}
+		}
+
+
 
